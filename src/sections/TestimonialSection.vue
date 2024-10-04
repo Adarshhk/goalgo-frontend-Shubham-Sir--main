2888da3a87ac7bdf40671a5f51960e3e2bf1cd12
Adarsh Kaushal <Adarshkaushal14@gmail.com>
2024-10-04 17:32:59 +0530

    final ui fixed

--- src/sections/TestimonialSection.vue
+++ src/sections/TestimonialSection.vue
@@ -42,21 +42,21 @@ const testimonals = [
           questions or issues you may have.
         </p>
       </div>
-      <div class="xl:w-[45%] flex flex-row justify-evenly overflow-hidden relative" >
+      <div class="xl:w-[45%] w-full   flex flex-row justify-evenly overflow-hidden relative" >
         <div :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
           class="transition-transform duration-500 min-w-full font-Inter" v-for='(testimonial, index) in testimonals'
           :key='index'>
-          <div class='p-8 rounded-2xl bg-white bg-opacity-10 border border-white border-opacity-25 space-y-5'>
+          <div class='lg:p-8 md:p-8 p-3 rounded-2xl bg-white bg-opacity-10 border border-white border-opacity-25 space-y-5'>
             <div class="flex items-start gap-4">
               <img src="../assets/images/svg/dummy.svg" alt="" />
 
               <div class="">
                 <p class="font-[19px]">{{ testimonial.user }}</p>
-                <p class="font-light text-[#8C8C8C] text-[16px]">Invester</p>
+                <p class="font-light text-[#8C8C8C] md:text-[16px] lg:text-[16px] text-[14px]">Invester</p>
               </div>
             </div>
 
-            <p class="font-normal text-[22px] pt-4">
+            <p class="font-normal md:text-[22px] lg:text-[22px] text-[15px] pt-4">
               {{ testimonial.message }}
             </p>
           </div>
@@ -67,6 +67,49 @@ const testimonals = [
     </div>
   </section>
 </template>
+
+<!-- <template>
+  <section class="w-full p-4 md:p-6 lg:p-10 font-Inter relative max-w-full mx-auto">
+    <div class="glow-effect hidden xl:block"></div>
+    <div class="z-10 flex flex-col xl:flex-row items-center justify-between space-y-10 md:space-y-14 xl:space-y-0 xl:py-20">
+      <div class="space-y-4 md:space-y-6 w-full xl:w-[50%] text-center xl:text-left">
+        <p class="text-algo-orange text-[18px] md:text-[20px]">Testimonials</p>
+        <h1 class="text-[32px] md:text-[48px] xl:text-[64px] leading-[40px] md:leading-[55px] xl:leading-[65px]">
+          Don't take <br />
+          <span class="text-algo-orange">our word for it</span>
+        </h1>
+        <p class="text-[#8C8C8C] text-[14px] md:text-[16px] font-light md:w-[80%] xl:w-full mx-auto xl:mx-0">
+          Our support team is available around the clock to help you with any questions or issues you may have.
+        </p>
+      </div>
+      <div class="xl:w-[45%] flex flex-row justify-start md:justify-evenly overflow-hidden relative">
+        <div
+          :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+          class="transition-transform duration-500 min-w-full font-Inter flex"
+        >
+          <div
+            v-for="(testimonial, index) in testimonals"
+            :key="index"
+            class="min-w-full p-6 md:p-8 rounded-2xl bg-white bg-opacity-10 border border-white border-opacity-25 space-y-4 md:space-y-5"
+          >
+            <div class="flex items-start gap-4">
+              <img class="w-12 h-12 md:w-14 md:h-14" src="../assets/images/svg/dummy.svg" alt="User Image" />
+
+              <div>
+                <p class="text-[16px] md:text-[19px]">{{ testimonial.user }}</p>
+                <p class="font-light text-[#8C8C8C] text-[12px] md:text-[16px]">Investor</p>
+              </div>
+            </div>
+
+            <p class="font-normal text-[16px] md:text-[22px] pt-2 md:pt-4">
+              {{ testimonial.message }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template> -->
 
 <style scoped>
 .glow-effect {
